3688fe2fc517ca3113f55bf76f78e1624c7d9348
typh01 <typh01@naver.com>
2025-01-20 11:25:24 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,42 @@
+// JS와 주석 동일
+
+/*
+ * Run 버튼 (단축키 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java) 
+ * 	 -> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class 파일명)
+*/
+
+/*
+ * .metadata 폴더 : 이클립스 설정 모음 폴더
+ * 
+ * .classpath : 클래스 경로 인식 방법 설정
+ * 
+ * bin(binary)폴더 : 컴파일된 파일(byte 코드) 모인 폴더
+ * 
+ * .class 파일 : 컴파일된 파일(바이트 코드)
+ * 
+ * */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("이클립스 실행 중");
+		System.out.println("어쩔 수 없네....");
+		System.out.println("연습만이 살 길이다.");
+		
+		/* Java API DOC: 
+		 * 	Java 에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 1) google -> java 21 api 검색
+		 * 
+		 * 2) 궁금한 코드 클릭 -> shift + f2
+		*/
+		
+		
+	}
+
+}
---
+++ JavaTest.java
@@ -0,0 +1,14 @@
+public class JavaTest{
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!!!");
+		System.out.println("안-----녕!!!!!!");
+		System.out.println("왜 않 금???");
+
+		for(int i=1; i<=5; i++){
+			System.out.println(i + "번째 줄 출력");
+		}
+	}
+
+}
